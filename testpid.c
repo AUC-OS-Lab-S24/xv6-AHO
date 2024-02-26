2549af32bcb066a9a092dde0fb97c2e9c9c85b2d
omarelfouly <omarelfouly@aucegypt.edu>
2024-02-27 01:45:43 +0200

    uncommeted testpid

--- testpid.c
+++ testpid.c
@@ -6,12 +6,12 @@ int
 main(int argc, char *argv[])
 {
   printf(1, "Process pid: %d\n", getpid());
-  // char myname[16];
-  // getpname(getpid());
-  // printf(1, "Process name: %s\n", myname);
-  // printf(1, "Parent pid: %d\n", getparentpid());
-  // char parentname[16];
-  // getpname(getparentpid());
-  // printf(1, "Parent name: %s\n", parentname);
+  char myname[16];
+  getpname(getpid());
+  printf(1, "Process name: %s\n", myname);
+  printf(1, "Parent pid: %d\n", getparentpid());
+  char parentname[16];
+  getpname(getparentpid());
+  printf(1, "Parent name: %s\n", parentname);
   exit();
 }
