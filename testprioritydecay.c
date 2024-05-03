dfcb2a307bcfcc77d9aa7dbb1e8ce06e81879d89
Ahmed-Waseem77 <ahmedwaseemr@aucegypt.edu>
2024-05-03 21:59:18 +0300

    priority decay

--- testprioritydecay.c
+++ testprioritydecay.c
@@ -5,17 +5,16 @@
 // we want to test the priority of the processes.
 // to set priorities we use the custom setpriority(pid, priority) syscall
 // to check processes table we use printptable() syscall
-
 void create_process(int n) {
     if (n > 0) {
         int pid = fork();
         setpriority(pid, n);
         if (pid == 0) {
-            sleep(100);
             // if process is even sleep more (for the priority to decay)
-            if (n % 2 == 0) {
-                sleep(3000);
+            for (int i = 0; i < 3; i++) {
+                int x = i * i;
             }
+
             printf(1, "\n\nThis is child process with id %d\n", getpid());
 
             printptable();
@@ -30,11 +29,10 @@ void create_process(int n) {
 
 int main(int argc, char *argv[])
 {
-
     // check if the number of arguments is correct
-    if (argc != 3)
+    if (argc != 2)
     {
-        printf(1, "Usage: testpriority <process count> <modulus>\n tip: try 10 5\n another tip: Have your process count higher than the modulus to see effect\n");
+        printf(1, "Usage: testpriority <process count> <modulus>\n tip: try 10 \n");
         exit();
     }
 
